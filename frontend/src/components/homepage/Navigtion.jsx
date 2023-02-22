6bd5ee3aa38964cfdf17b8c864fd98aee41a388f
mdhyan.99@gmail.com <mdhyan.99@gmail.com>
2023-02-22 15:36:47 +0100

    title pms

--- frontend/src/components/homepage/Navigtion.jsx
+++ frontend/src/components/homepage/Navigtion.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import './home.scss'
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "./home.scss";
 const Navigtion = () => {
-  return (
-   <div className='navi'>
-     <div className='navigtion'>
-        <div>Home Page</div>
-        <div>ToDo List</div>
-        <div className='pms'>PMS </div>
-        <div>Notiz</div>
-        <div>Calendar</div>
-    </div>
-    <div className='start'>
-        Startseite
-    </div>
-   </div>
+    return (
+        <div className="navi">
+            <div className="navigtion">
+                <div>Home Page</div>
+                <div>ToDo List</div>
+                <a title="Products Management System">PMS </a>
+                <div>Notiz</div>
+                <div>Calendar</div>
+            </div>
+            <div className="start">Startseite</div>
+        </div>
+    );
+};
 
-  )
-}
-
-export default Navigtion
+export default Navigtion;
