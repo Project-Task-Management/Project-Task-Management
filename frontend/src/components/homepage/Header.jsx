12ac590b486db70db1d1a507c3a2ce962956a298
mdhyan.99@gmail.com <mdhyan.99@gmail.com>
2023-02-23 11:37:07 +0100

    ss

--- frontend/src/components/homepage/Header.jsx
+++ frontend/src/components/homepage/Header.jsx
@@ -1,21 +1,18 @@
+import React from "react";
+import { Button } from "react-bootstrap";
 
-import React from 'react';
-import { Button, Nav } from "react-bootstrap";
-import Container from "react-bootstrap/Container";
-import Navbar from "react-bootstrap/Navbar";
-
-import './home.scss'
+import "./home.scss";
 
 const Header = () => {
-  return (
-    
-      <div className="header">
-      <div>하자 LETSDO</div>
-        <div>Logo</div>
-        <div><Button>Registieren</Button></div>
-      </div>
-
-  );
-}
+    return (
+        <div className="header">
+            <div>하자 LETSDO</div>
+            <div>Logo</div>
+            <div>
+                <Button>Registieren</Button>
+            </div>
+        </div>
+    );
+};
 
 export default Header;
--- frontend/src/components/homepage/Home.jsx
+++ frontend/src/components/homepage/Home.jsx
@@ -1,7 +1,4 @@
-
 import React from "react";
-import Header from "./Header";
-import Navigtion from "./Navigtion";
 import './home.scss'
 
 export default function Home() {
