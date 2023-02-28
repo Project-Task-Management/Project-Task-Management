3df978e65f015169c90f2bf1bfbd1d71c240a1f2
mohsenreza1 <rezaimohsen424@gmail.com>
2023-02-28 16:00:31 +0100

    notizen löschen  fertig gestellt

--- frontend/src/components/notiz/Notiz.jsx
+++ frontend/src/components/notiz/Notiz.jsx
@@ -1,82 +1,66 @@
-
 import { useState } from "react";
 import "./Notiz.scss";
 
 import Button from "./bearbeiten/Button";
 
-
 export default function Notiz() {
-
   const [headline, setHeadline] = useState("");
-  const [notiz, setNotiz] = useState([])
+  const [notiz, setNotiz] = useState([]);
   const [text, setText] = useState("");
 
-  
-
   const clickHandler = (e) => {
-    e.preventDefault()
-    const newNotiz = [...notiz, {headline, text}]
-    setNotiz(newNotiz)
-    setText("")
-    setHeadline("")
-    console.log("gespeichert")
-  }
+    e.preventDefault();
+    const newNotiz = [...notiz, { headline, text }];
+    setNotiz(newNotiz);
+    setText("");
+    setHeadline("");
+    console.log("gespeichert");
+  };
 
- 
+  const deleteFunction = (i) => {
+    console.log(i, "gelöcht");
+  };
 
-  const headlineChangehandle = (e) =>{
-    setHeadline(e.target.value)
-  }
-
-
+  const headlineChangehandle = (e) => {
+    setHeadline(e.target.value);
+  };
 
   const textChangehandle = (e) => {
-    setText(e.target.value)
-  }
+    setText(e.target.value);
+  };
+  deleteFunction(3);
+  return (
+    <>
+      <h1>Schreiben sie ihr Notizen </h1>
 
-return (
-  <>
-    <h1>Schreiben sie ihr Notizen </h1>
+      <form>
+        <input
+          type="text"
+          value={headline}
+          onChange={headlineChangehandle}
+          placeholder=" Dein Überschrift "
+        />
 
-    <form>
-    <input 
-    type="text"
-    id="überschrifft"
-    value={headline}
-    onChange={headlineChangehandle}
-    placeholder=" Dein Überschrift "
-    />
-    
-    <textarea
-    type="text"
-    id="text"
-    value={text} 
-    onChange={textChangehandle}
-    placeholder="Dein Notitzen"
-    />
+        <textarea
+          type="text"
+          value={text}
+          onChange={textChangehandle}
+          placeholder="Dein Notitzen"
+        />
 
-    <button  onClick={clickHandler}>Speichern</button>
+        <button onClick={clickHandler}>Speichern</button>
+      </form>
+      <div className="border"></div>
 
-    </form>
-    <div className="border"></div>
-
-
-  <ol>
-    {notiz.map((elem, i) => 
-    <li key={i}>
-      <h2>{elem.headline}</h2>
-      <p>{elem.text}</p>
-      <Button />
-
-      </li>
-    )} 
-    </ol> 
-      
-
-
-  </>
-)
-
+      <ol>
+        {notiz.map((elem, i) => (
+          <li key={i}>
+            <h2>{elem.headline}</h2>
+            <p>{elem.text}</p>
+            <Button handleDellete={() => deleteFunction(i)} />
+          </li>
+        ))}
+      </ol>
+    </>
+  );
 }
-
-
