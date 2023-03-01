afbfe48ebf5b7b07374648dfbed22b18d01c17b8
monsif <monsif@gmail.com>
2023-03-01 10:52:15 +0100

    push

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-=======
-import Title from "./components/todo/Title";
-import React from "react";
-import AppHeader from "./components/todo/AppHeader";
-import Appcontent from "./components/todo/Appcontent";
-
-
-function App() {
-    return (
-        <div className="container">
-            <Title>Todo List</Title>
-            <div className="app__wrapper"></div>
-            <AppHeader></AppHeader>
-            <Appcontent></Appcontent>
-        </div>
-    );
-}
->>>>>>> monsif
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./App.css";
@@ -37,22 +17,21 @@ function App() {
                 <Header />
                 <div className="all">
                     <div className="navo">
-                    <Navigtion />
+                        <Navigtion />
                     </div>
                     <div className="rout">
-                    <Routes >
-                        <Route path="/" element={<Home />}></Route>
-                        <Route path="/kalender" element={<Kalender />}></Route>
-                        <Route path="/notiz" element={<Notiz />}></Route>
-                       <Route path="/products" element={<Products />}></Route>
-            <Route path="/todo" element={<Todo />}></Route>
-            <Route path="/registierung" element={<Registierung />}></Route>
-                    </Routes>
+                        <Routes>
+                            <Route path="/" element={<Home />}></Route>
+                            <Route path="/kalender" element={<Kalender />}></Route>
+                            <Route path="/notiz" element={<Notiz />}></Route>
+                            <Route path="/products" element={<Products />}></Route>
+                            <Route path="/todo" element={<Todo />}></Route>
+                            <Route path="/registierung" element={<Registierung />}></Route>
+                        </Routes>
                     </div>
                 </div>
             </BrowserRouter>
         </div>
     );
-
-    }
+}
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./App"
 import 'bootstrap/dist/css/bootstrap.min.css';
-=======
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
->>>>>>> yaman
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
