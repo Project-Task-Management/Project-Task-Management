2b92223f6a6c8f579f5fb3d766d942fc6ca68ced
yaaroub <yarobdaoud1997@gmail.com>
2023-03-07 17:42:13 +0100

    search input

--- frontend/src/components/ProductsManagementSystem/Create.jsx
+++ frontend/src/components/ProductsManagementSystem/Create.jsx
@@ -1,12 +1,18 @@
 import Delete from "./Delete.jsx";
-
+import { useState } from "react";
 import DeleteAll from "./DeleteAll.jsx";
 export default function ({ value, total, setValue, setData, data }) {
+  const [searchInput, setSearchInput] = useState("");
   const clickHandler = () => {
     const newEntry = { ...value, total };
     const neuData = [...data, newEntry];
-    if (value.price === "") {
-      return alert("Price bitte eingeben");
+    if (
+      value.price === "" ||
+      value.count === "" ||
+      value.price === 0 ||
+      value.count === 0
+    ) {
+      return alert("Price oder Count bitte eingeben");
     } else setData(neuData);
     setValue({
       title: "",
@@ -19,9 +25,30 @@ export default function ({ value, total, setValue, setData, data }) {
     });
   };
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value.toLowerCase());
+  };
+
+  const filteredData = data.filter((el) => {
+    if (searchInput === "") {
+      return el;
+    } else {
+      return el.title.toLowerCase().includes(searchInput);
+    }
+  });
+  const calc = () => {};
   return (
     <>
       <button onClick={clickHandler}>Create</button>
+
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Search"
+        value={searchInput}
+      />
+
       {data.length > 0 ? <DeleteAll data={data} setData={setData} /> : ""}
       <table>
         <thead>
@@ -33,33 +60,38 @@ export default function ({ value, total, setValue, setData, data }) {
             <th>ADS</th>
             <th>DISCOUNT</th>
             <th>TOTAL</th>
-            <th>COUNT</th>
+
             <th>CATEGORY</th>
             <th>DELETE</th>
           </tr>
         </thead>
-        {data.map((e, i) => {
-          return (
-            <tbody key={i}>
-              <tr>
-                <td>{i + 1}</td>
+        <tbody>
+          {filteredData.map((e, i) => {
+            const rows = [];
+            for (let index = i; index < e.count; index++) {
+              rows.push(
+                <tr key={i}>
+                  
 
-                <td>{e.title}</td>
-                <td>{e.price}</td>
-                <td>{e.taxes}</td>
-                <td>{e.ads}</td>
-                <td>{e.discount}</td>
-                <td>{e.total}</td>
-                <td>{e.count}</td>
-                <td>{e.category}</td>
+                  <td>{e.title}</td>
+                  <td>{e.price}</td>
+                  <td>{e.taxes}</td>
+                  <td>{e.ads}</td>
+                  <td>{e.discount}</td>
+                  <td>{e.total}</td>
 
-                <td>
-                  <Delete data={data} setData={setData} />
-                </td>
-              </tr>
-            </tbody>
-          );
-        })}
+                  <td>{e.category}</td>
+
+                  <td>
+                    <Delete data={data} setData={setData} />
+                  </td>
+
+                </tr>
+              );
+            }
+            return rows;
+          })}
+        </tbody>
       </table>
     </>
   );
