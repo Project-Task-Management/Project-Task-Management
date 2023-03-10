29124114cc1a210912151549c5ee08ab828768bd
yaaroub <yarobdaoud1997@gmail.com>
2023-03-10 20:15:52 +0100

    craete und save data in backend

--- frontend/src/components/ProductsManagementSystem/Create.jsx
+++ frontend/src/components/ProductsManagementSystem/Create.jsx
@@ -1,19 +1,28 @@
 import Delete from "./Delete.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { getProducts } from "../../api.js";
 import DeleteAll from "./DeleteAll.jsx";
 export default function ({ value, total, setValue, setData, data }) {
   const [searchInput, setSearchInput] = useState("");
   const clickHandler = () => {
     const newEntry = { ...value, total };
     const neuData = [...data, newEntry];
-    if (
-      value.price === "" ||
-      value.count === "" ||
-      value.price === 0 ||
-      value.count === 0
-    ) {
+    if (value.price < 0 || value.count < 0) {
       return alert("Price oder Count bitte eingeben");
-    } else setData(neuData);
+    } else {
+      setData(neuData);
+      axios.post("http://localhost:7897/product/", {
+        title: value.title,
+        price: value.price,
+        taxes: value.taxes,
+        ads: value.ads,
+        discount: value.discount,
+        total: value.total,
+        count: value.count,
+        category: value.category,
+      });
+    }
     setValue({
       title: "",
       price: "",
@@ -37,7 +46,7 @@ export default function ({ value, total, setValue, setData, data }) {
       return el.title.toLowerCase().includes(searchInput);
     }
   });
-  const calc = () => {};
+
   return (
     <>
       <button onClick={clickHandler}>Create</button>
@@ -67,25 +76,25 @@ export default function ({ value, total, setValue, setData, data }) {
         </thead>
         <tbody>
           {filteredData.map((e, i) => {
+            console.log(filteredData);
             const rows = [];
-            for (let index = i; index < e.count; index++) {
+            for (let index = 0; index < e.count; index++) {
               rows.push(
                 <tr key={i}>
-                  
-
+                  <td>
+                    {i + 1}-{index + 1}
+                  </td>
                   <td>{e.title}</td>
                   <td>{e.price}</td>
                   <td>{e.taxes}</td>
                   <td>{e.ads}</td>
                   <td>{e.discount}</td>
                   <td>{e.total}</td>
-
                   <td>{e.category}</td>
 
                   <td>
-                    <Delete data={data} setData={setData} />
+                    <Delete rows={rows} data={data} setData={setData} />
                   </td>
-
                 </tr>
               );
             }
