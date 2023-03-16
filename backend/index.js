a027af72df299d65c531a91f72b419844bac0a9c
Yaman Al Haj Dawoud <yamandaoud2000@gmail.com>
2023-03-16 13:10:29 +0100

    removehandle

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,21 @@
 import dotenv from "dotenv";
 dotenv.config();
-import pmsRouter from "./routers/pmsRouter.js"
+import pmsRouter from "./routers/pmsRouter.js";
 import "./lib/mongoose.js";
-import cors from "cors"
+import cors from "cors";
+import kalanderRouter from "./routers/kalanderRouter.js";
 
 import express from "express";
 const app = express();
 
 const port = process.env.PORT || 4000;
-app.use(cors())
-
+app.use(cors());
 
 app.use(express.json());
-app.use("/product",pmsRouter)
+app.use("/product", pmsRouter);
 
+app.use("/kalander", kalanderRouter);
 
-
-app.listen(port,()=>{
-    console.log(port,"i am on running");
-})
+app.listen(port, () => {
+  console.log(port, "i am on running");
+});
--- backend/routers/pmsRouter.js
+++ backend/routers/pmsRouter.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import * as controller from "../controllers/pmsController.js"
-const router = Router()
+import * as controller from "../controllers/pmsController.js";
+const router = Router();
 router
-        .get("/",controller.getAllProducts)
-        .get("/:productId",controller.getProduct)
-        .post("/",controller.createProduct)
-        .patch("/:productId",controller.updateProduct)
-        
-        .delete("/:productId",controller.deleteProduct)
+  .get("/", controller.getAllProducts)
+  .get("/:productId", controller.getProduct)
+  .post("/", controller.createProduct)
+  .patch("/:productId", controller.updateProduct)
 
-export default router
+  .delete("/:productId", controller.deleteProduct);
+
+export default router;
