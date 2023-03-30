3313499cad7c96a8b26941c95209cf43c4a57f9e
mdhyan.99@gmail.com <mdhyan.99@gmail.com>
2023-03-30 04:02:46 +0200

    hom css

--- frontend/src/components/homepage/Home.jsx
+++ frontend/src/components/homepage/Home.jsx
@@ -17,17 +17,18 @@ export default function Home() {
                         reiciendis perferendis voluptatibus mollitia!
                     </span>
                 </div>
-                <div>
+              
+            </main>
+            <div>
                     <img
                         className="beschreib_img"
-                        src="https://img.freepik.com/free-vector/project-management-goal-completion-list-questionnaire-survey-answering-business-organization-tool_335657-3289.jpg"
+                        src="https://www.frevvo.com/blog/wp-content/uploads/2021/12/image5-3.png"
                         alt=""
                     />
                 </div>
-            </main>
-            <section className="beschreib">
+                <main className="beschreib">
                 <div className="beschreib_text">
-                    <h2 className="beschreib_h2">Was wir anbieten</h2>
+                    <h2 className="beschreib_h2">Was wir bieten</h2>
                     <span className="beschreib_span">
                         Lorem ipsum dolor, sit amet consectetur adipisicing
                         elit. <br /> Placeat soluta explicabo dolor corporis
@@ -36,54 +37,31 @@ export default function Home() {
                         reiciendis perferendis voluptatibus mollitia!
                     </span>
                 </div>
-                <div>
-                    {" "}
+              
+            </main>
+            <section className="beschrei">
+             
+                <div className="notiz-todo-kalender">
+                   
                     <img
-                        className="beschreib_img"
-                        src="https://www.ntaskmanager.com/wp-content/uploads/2021/02/Task-management-vs-project-management.jpg"
+                        className="beschreib_imges"
+                        src="https://as2.ftcdn.net/v2/jpg/00/21/99/83/1000_F_21998361_jnNu0OQi46xnhdFw0pPPWpGAQo1Qg7iG.jpg"
+                        alt=""
+                    />
+                       <img
+                        className="beschreib_imges"
+                        src="https://static.vecteezy.com/ti/gratis-vektor/p1/4718992-komplette-to-do-liste-erledigen-projekt-planen-oder-fertigstellen-aufgaben-im-zeitplan-hilfe-das-geschaftsziel-und-ziel-erledigt-geschaftsfrau-aufsteigen-todo-liste-to-hakchen-beendetes-arbeiten-projekt-aufgaben-checkbox-vektor.jpg"
+                        alt=""
+                    />
+                       <img
+                        className="beschreib_imges"
+                        src="https://werner-zenk.de/archiv/img/event-kalender_sqlite.png"
                         alt=""
                     />
                 </div>
             </section>
-            <main className="beschreib">
-                <div className="beschreib_text">
-                    <h2 className="beschreib_h2">Über uns</h2>
-                    <span className="beschreib_span">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. <br /> Placeat soluta explicabo dolor corporis
-                        illo voluptates qui iure et, neque esse! <br />{" "}
-                        Reprehenderit exercitationem magnam enim quasi nam
-                        reiciendis perferendis voluptatibus mollitia!
-                    </span>
-                </div>
-                <div>
-                    <img
-                        className="beschreib_img"
-                        src="https://img.freepik.com/free-vector/project-management-goal-completion-list-questionnaire-survey-answering-business-organization-tool_335657-3289.jpg"
-                        alt=""
-                    />
-                </div>
-            </main>
-            <section className="beschreib">
-                <div className="beschreib_text">
-                    <h2 className="beschreib_h2">Was wir anbieten</h2>
-                    <span className="beschreib_span">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. <br /> Placeat soluta explicabo dolor corporis
-                        illo voluptates qui iure et, neque esse! <br />{" "}
-                        Reprehenderit exercitationem magnam enim quasi nam
-                        reiciendis perferendis voluptatibus mollitia!
-                    </span>
-                </div>
-                <div>
-                    {" "}
-                    <img
-                        className="beschreib_img"
-                        src="https://www.ntaskmanager.com/wp-content/uploads/2021/02/Task-management-vs-project-management.jpg"
-                        alt=""
-                    />
-                </div>
-            </section>
+            
+            
         </div>
     );
 }
