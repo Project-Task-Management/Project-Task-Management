f668119079d1d9d9d63e19622a96e0b5a005214e
yaaroub <yarobdaoud1997@gmail.com>
2023-04-04 16:17:39 +0200

    css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/homepage/Header";
 import Home from "./components/homepage/Home";
-import Navigtion from "./components/homepage/Navigtion";
 import Kalender from "./components/kalender/Kalender";
 import Notiz from "./components/notiz/Notiz";
 import Todo from "./components/todo/ToDo";
@@ -16,31 +15,28 @@ import Footer from "./components/homepage/Footer";
 function App() {
   return (
     <TodoProvider>
-        <BrowserRouter>
-          <div className="app_header">
-            <Header />
-          </div>
-      <div className="app_navo-rout">
-
-          <div className="app_navo">
-            <Navigtion />
-          </div>
+      <BrowserRouter>
+        <div className="app_header">
+          <Header />
+        </div>
+        <div className="app_navo-rout">
+          <div className="app_navo"></div>
           <div className="app_rout">
-         <div className="rout">
-         <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/kalender" element={<Kalender />}></Route>
-              <Route path="/notiz" element={<Notiz />}></Route>
-              <Route path="/products" element={<Products />}></Route>
-              <Route path="/todo" element={<Todo />}></Route>
-              <Route path="/registierung" element={<Registierung />}></Route>
-              <Route path="/anmeldung" element={<Anmeldung />}></Route>
-            </Routes>
-         </div>
-          <Footer />
+            <div className="rout">
+              <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/kalender" element={<Kalender />}></Route>
+                <Route path="/notiz" element={<Notiz />}></Route>
+                <Route path="/products" element={<Products />}></Route>
+                <Route path="/todo" element={<Todo />}></Route>
+                <Route path="/registierung" element={<Registierung />}></Route>
+                <Route path="/anmeldung" element={<Anmeldung />}></Route>
+              </Routes>
+            </div>
+            <Footer />
           </div>
-      </div>
-        </BrowserRouter>
+        </div>
+      </BrowserRouter>
     </TodoProvider>
   );
 }
--- frontend/src/components/homepage/Home.jsx
+++ frontend/src/components/homepage/Home.jsx
@@ -2,66 +2,58 @@ import React from "react";
 import "./home.scss";
 
 export default function Home() {
-    return (
-        <div className="hom">
-            <h1 className="start">Willkommen in unsere Webseite</h1>
+  return (
+    <div className="hom">
+      <h1 className="start">Willkommen in unsere Webseite</h1>
 
-            <main className="beschreib">
-                <div className="beschreib_text">
-                    <h2 className="beschreib_h2">Über uns</h2>
-                    <span className="beschreib_span">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. <br /> Placeat soluta explicabo dolor corporis
-                        illo voluptates qui iure et, neque esse! <br />{" "}
-                        Reprehenderit exercitationem magnam enim quasi nam
-                        reiciendis perferendis voluptatibus mollitia!
-                    </span>
-                </div>
-              
-            </main>
-            <div>
-                    <img
-                        className="beschreib_img"
-                        src="https://www.frevvo.com/blog/wp-content/uploads/2021/12/image5-3.png"
-                        alt=""
-                    />
-                </div>
-                <main className="beschreib">
-                <div className="beschreib_text">
-                    <h2 className="beschreib_h2">Was wir bieten</h2>
-                    <span className="beschreib_span">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing
-                        elit. <br /> Placeat soluta explicabo dolor corporis
-                        illo voluptates qui iure et, neque esse! <br />{" "}
-                        Reprehenderit exercitationem magnam enim quasi nam
-                        reiciendis perferendis voluptatibus mollitia!
-                    </span>
-                </div>
-              
-            </main>
-            <section className="beschrei">
-             
-                <div className="notiz-todo-kalender">
-                   
-                    <img
-                        className="beschreib_imges"
-                        src="https://as2.ftcdn.net/v2/jpg/00/21/99/83/1000_F_21998361_jnNu0OQi46xnhdFw0pPPWpGAQo1Qg7iG.jpg"
-                        alt=""
-                    />
-                       <img
-                        className="beschreib_imges"
-                        src="https://static.vecteezy.com/ti/gratis-vektor/p1/4718992-komplette-to-do-liste-erledigen-projekt-planen-oder-fertigstellen-aufgaben-im-zeitplan-hilfe-das-geschaftsziel-und-ziel-erledigt-geschaftsfrau-aufsteigen-todo-liste-to-hakchen-beendetes-arbeiten-projekt-aufgaben-checkbox-vektor.jpg"
-                        alt=""
-                    />
-                       <img
-                        className="beschreib_imges"
-                        src="https://werner-zenk.de/archiv/img/event-kalender_sqlite.png"
-                        alt=""
-                    />
-                </div>
-            </section>
-            
-            
+      <main className="beschreib">
+        <div className="beschreib_text">
+          <h2 className="beschreib_h2">Über uns</h2>
+          <span className="beschreib_span">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
+            Placeat soluta explicabo dolor corporis illo voluptates qui iure et,
+            neque esse! <br /> Reprehenderit exercitationem magnam enim quasi
+            nam reiciendis perferendis voluptatibus mollitia!
+          </span>
         </div>
-    );
+      </main>
+      <div>
+        <img
+          className="beschreib_img"
+          src="https://www.frevvo.com/blog/wp-content/uploads/2021/12/image5-3.png"
+          alt=""
+        />
+      </div>
+      <main className="beschreib">
+        <div className="beschreib_text">
+          <h2 className="beschreib_h2">Was wir bieten</h2>
+          <span className="beschreib_span">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />{" "}
+            Placeat soluta explicabo dolor corporis illo voluptates qui iure et,
+            neque esse! <br /> Reprehenderit exercitationem magnam enim quasi
+            nam reiciendis perferendis voluptatibus mollitia!
+          </span>
+        </div>
+      </main>
+      <section className="beschrei">
+        <div className="notiz-todo-kalender">
+          <img
+            className="beschreib_imges"
+            src="https://as2.ftcdn.net/v2/jpg/00/21/99/83/1000_F_21998361_jnNu0OQi46xnhdFw0pPPWpGAQo1Qg7iG.jpg"
+            alt=""
+          />
+          <img
+            className="beschreib_imges"
+            src="https://static.vecteezy.com/ti/gratis-vektor/p1/4718992-komplette-to-do-liste-erledigen-projekt-planen-oder-fertigstellen-aufgaben-im-zeitplan-hilfe-das-geschaftsziel-und-ziel-erledigt-geschaftsfrau-aufsteigen-todo-liste-to-hakchen-beendetes-arbeiten-projekt-aufgaben-checkbox-vektor.jpg"
+            alt=""
+          />
+          <img
+            className="beschreib_imges"
+            src="https://werner-zenk.de/archiv/img/event-kalender_sqlite.png"
+            alt=""
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
